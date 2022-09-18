1babfdc7557dfa5e557c29d38eff93771a456005
Faisal Ardhi <84627910+Faisal-PEA@users.noreply.github.com>
2022-09-18 22:28:29 +0700

    Update view_login.blade.php

--- resources/views/login/view_login.blade.php
+++ resources/views/login/view_login.blade.php
@@ -9,12 +9,12 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('/')}}plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ secure_asset('/')}}plugins/fontawesome-free/css/all.min.css">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('/')}}plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ secure_asset('/')}}plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('/')}}dist/css/adminlte.min.css">
-    <link rel="shortcut icon" href="{{asset('/')}}dist/img/pngegg.png">
+    <link rel="stylesheet" href="{{ secure_asset('/')}}dist/css/adminlte.min.css">
+    <link rel="shortcut icon" href="{{secure_asset('/')}}dist/img/pngegg.png">
 </head>
 
 <body class="hold-transition login-page">
@@ -78,11 +78,11 @@
     <!-- /.login-box -->
 
     <!-- jQuery -->
-    <script src="{{ asset('/')}}plugins/jquery/jquery.min.js"></script>
+    <script src="{{ secure_asset('/')}}plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('/')}}plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ secure_asset('/')}}plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('/')}}dist/js/adminlte.min.js"></script>
+    <script src="{{ secure_asset('/')}}dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
